0edd66ffd16dbeff52cc552a98833508d615156c
wisniewskipiotr <piotr.t.wisniewski@gmail.com>
2020-11-08 23:20:36 +0100

    Completion of basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -10,6 +12,10 @@ public class PlayerController : MonoBehaviour
     public CharacterController CharController;
 
     private Vector3 moveInput;
+
+    public Transform camPlayer;
+
+    public float sensitivity;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +25,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //First version of movemnet control
 
-        CharController.Move(moveInput);
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        Vector3 verticalMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horizontalMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = verticalMove + horizontalMove;
+        moveInput.Normalize();
+        moveInput = moveInput * moveSpeed ;
+
+        CharController.Move(moveInput * Time.deltaTime);
+
+        //camera rotation
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * sensitivity;
 
 
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        // same thing asd above but different method(and it also works:))
+
+        camPlayer.rotation = Quaternion.Euler(camPlayer.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
